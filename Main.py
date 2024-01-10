54ed3b5ee08ef7e60cf41a03d2f7d85963561a94
harshit d r <harshitkumarabc@gmail.com>
2024-01-10 18:22:32 +0530

    resized

--- Main.py
+++ Main.py
@@ -210,12 +210,21 @@ class LoginApp(MDApp):
         screen_manager.add_widget(ServiceProvider("service_provider"))
         screen_manager.add_widget(ServiceRegister("service_register_form"))
         screen_manager.add_widget(Builder.load_file("slot_booking.kv"))
+        screen_manager.add_widget(Builder.load_file("payment_page.kv"))
         screen_manager.add_widget(ServiceRegisterGym("gym_register_form"))
         screen_manager.add_widget(ServiceRegisterAmbulance("ambulance_register_form"))
         screen_manager.add_widget(ServiceProviderMain(name="service_provider_main_page"))
 
         return screen_manager
 
+    #dialog box
+    def show_validation_dialog(self, message):
+        # Display a dialog for invalid login or sign up
+        dialog = MDDialog(
+            text=message,
+            buttons=[MDFlatButton(text="OK", on_release=lambda x: dialog.dismiss())],
+        )
+        dialog.open()
 
     #-------------------------service-provider-flow-------------
     menu = None
